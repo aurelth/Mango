b539032ff2927306396c9771852165b52c69baea
Aurel <aurelth@icloud.com>
2023-09-18 08:17:33 -0300

    Services Registration in Program.cs

--- Mango.Web/Service/BaseService.cs
+++ Mango.Web/Service/BaseService.cs
@@ -72,6 +72,7 @@ namespace Mango.Web.Service
                     Message = ex.Message.ToString(),
                     IsSuccess = false
                 };
+
                 return dto;
             }            
         }
